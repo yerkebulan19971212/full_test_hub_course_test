b752af6c07b3691601d7c40f15967a8021740c94
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2023-08-28 18:11:25 +0600

    image field '

--- src/common/serializers/common.py
+++ src/common/serializers/common.py
@@ -10,7 +10,7 @@ class QuizzTypeSerializer(serializers.ModelSerializer):
     name_kz = serializers.CharField(source='quizz_type.name_kz')
     name_ru = serializers.CharField(source='quizz_type.name_ru')
     name_en = serializers.CharField(source='quizz_type.name_en')
-    icon = serializers.CharField(source='quizz_type.icon')
+    icon = serializers.ImageField(source='quizz_type.icon')
     color = serializers.CharField(source='quizz_type.color')
 
     class Meta:
