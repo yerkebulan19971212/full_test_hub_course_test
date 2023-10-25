4158b38054a757738fafca196fcb19ae8f040c1e
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2023-10-25 18:22:45 +0600

    student quizz filter

--- src/quizzes/admin.py
+++ src/quizzes/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import StudentQuizz
 
 admin.site.register([
-    StudentQuizz
 ])
+
+
+@admin.register(StudentQuizz)
+class StudentQuizzAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'id', 'course_type', 'packet', 'status', 'lesson',
+        'lesson_pair', 'quizz_type', 'language', 'quizz_start_time',
+        'quizz_end_time', 'created',
+        'modified')
+    fields = (
+        'user', 'id', 'course_type', 'packet', 'status', 'lesson',
+        'lesson_pair', 'quizz_type', 'language', 'quizz_start_time',
+        'quizz_end_time', 'created',
+        'modified')
+    readonly_fields = ('pk', 'created', 'modified')
+    search_fields = (
+        'user__email',
+        'user__username',
+        'user__phone',
+    )
