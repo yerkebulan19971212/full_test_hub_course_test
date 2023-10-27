ee7aecd99dbdbd541a28628c772f1a36fddc7702
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2023-10-27 14:17:23 +0600

    stuednt course

--- src/quizzes/api_views/full_test.py
+++ src/quizzes/api_views/full_test.py
@@ -262,11 +262,12 @@ class PassStudentAnswerView(generics.CreateAPIView):
                         student_quizz_id=student_quizz_id,
                         question=question
                     ).update(status=False)
-                    StudentScore.objects.filter(
+                    StudentScore.objects.get_or_create(
                         student_quizz_id=student_quizz_id,
                         question=question,
-                        score=score
-                    ).update(status=False)
+                        score=score,
+                        status=True
+                    )
 
             except Exception as e:
                 print(e)
