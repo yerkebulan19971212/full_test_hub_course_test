3dbb701d6f65a8450119545d5a596dca7fb99e60
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2023-11-08 15:38:14 +0600

    packet admin

--- src/common/admin.py
+++ src/common/admin.py
@@ -14,6 +14,37 @@ admin.site.register([
     KaspiPay,
     Lesson,
     LessonPair,
-    Packet,
     QuizzType
 ])
+
+
+@admin.register(Packet)
+class PacketAdmin(admin.ModelAdmin):
+    list_display = (
+        'name_kz',
+        'pk',
+        'name_ru',
+        'name_en',
+        'quizz_type',
+        'days',
+        'price',
+        'packet_type',
+        'quantity',
+        'created',
+        'modified'
+    )
+    fields = (
+        'name_kz',
+        'pk',
+        'name_ru',
+        'name_en',
+        'quizz_type',
+        'days',
+        'price',
+        'packet_type',
+        'quantity',
+        'created',
+        'modified'
+    )
+    readonly_fields = ('pk', 'created', 'modified')
+    search_fields = ('name_kz', 'name_ru', 'name_en')
