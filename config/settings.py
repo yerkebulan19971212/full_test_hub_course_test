846d1d6026ef06fb15c2a7f92839b4885ec83c9a
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2023-11-20 09:43:07 +0600

    django admin

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ CORS_ALLOWED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
