b211b30a72a8e015c5f4a3cf7754b7e93f54ff3d
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2024-01-10 11:24:18 +0600

    not local 2

--- config/celery.py
+++ config/celery.py
@@ -15,6 +15,14 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(7200.0, rating_quizz_full.s(),
                              name='add every 3600')
 
+    sender.add_periodic_task(7200.0, rating_quizz_full_2.s(),
+                             name='add every 3600')
+
+
+@celery_app.task
+def rating_quizz_full_2():
+    print('========')
+
 
 @celery_app.task
 def rating_quizz_full():
