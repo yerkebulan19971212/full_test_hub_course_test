c636e408634cd40b14e71d2eca90cb5aefd7903e
yabdilkakov <yerkebulan19971212@gmail.com>
2024-01-23 20:13:09 +0600

    jaazmin

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ CORS_ALLOWED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
