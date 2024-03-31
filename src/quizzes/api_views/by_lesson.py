499dc35925e1b6d51366f4d31ead106dbe1a26bc
yabdilkakov <yerkebulan19971212@gmail.com>
2024-03-31 13:11:50 +0500

    question lesson level

--- src/quizzes/api_views/by_lesson.py
+++ src/quizzes/api_views/by_lesson.py
@@ -189,7 +189,7 @@ class ByLessonPassAnswerView(generics.CreateAPIView):
                 with transaction.atomic():
                     score = 0
                     question = Question.objects.select_related(
-                        'lesson_question_level__question_level'
+                        'lesson_question_level__question_level',
                         'lesson_question_level__test_type_lesson__lesson',
                     ).get(pk=question_id)
                     lesson = question.lesson_question_level.test_type_lesson.lesson
